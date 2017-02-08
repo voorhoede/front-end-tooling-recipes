0a3e9e4d8616d5e26832dfe10aa7c0e50d45e4a1
Jasper Moelker <j.b.moelker@activeids.nl>
2017-02-08 14:05:18 +0100

    Rollup build and watch (#11)
    feat(rollup): build and watch
    
    with bundling, transpling, linting and minification

---
+++ rollup-bundle-and-watch/.eslintrc.js
@@ -0,0 +1,16 @@
+const isProduction = process.env.NODE_ENV;
+
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "parserOptions": {
+        "sourceType": "module"
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "no-console": isProduction ? ["error"] : ["off"],
+        "no-debugger": isProduction ? ["error"] : ["off"]
+    }
+};
---
+++ rollup-bundle-and-watch/src/modules/mod-b.js
@@ -0,0 +1,14 @@
+// borrowed from https://github.com/jlengstorf/learn-rollup/blob/master/src/scripts/modules/mod2.js
+
+/**
+ * Adds all the values in an array.
+ * @param  {Array} arr an array of numbers
+ * @return {Number}    the sum of all the array values
+ */
+const addArray = arr => {
+  const result = arr.reduce((a, b) => a + b, 0);
+
+  return result;
+};
+
+export default addArray;
