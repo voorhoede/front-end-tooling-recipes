8c228d11f380d6656e63791d2ce75059a376f0ac
Jasper Moelker <j.b.moelker@activeids.nl>
2017-11-01 10:23:21 +0100

    Use path module to resolve paths

--- revision-static-assets/scripts/revision.js
+++ revision-static-assets/scripts/revision.js
@@ -1,25 +1,27 @@
 const filter = require('gulp-filter')
 const gulp = require('gulp')
 const override = require('gulp-rev-css-url')
+const path = require('path')
 const rev = require('gulp-rev')
 const revDel = require('gulp-rev-delete-original')
 const revReplace = require('gulp-rev-replace')
 
-const workingDir = `${__dirname}/../dist/`
+const projectDir = path.join(__dirname, '..')
+const workingDir = path.join(projectDir, 'dist')
 const manifestFilename = 'rev-manifest.json'
 const pattern = /.*-[0-9a-f]{10}\..*/
 
 function revision () {
   return revisionHash()
     .on('end', () => revisionReplace())
-    .on('end', () => console.log(`Revisioned files in ${workingDir}`));
+    .on('end', () => console.log(`Revisioned files in ${ path.relative(projectDir, workingDir) }`));
 }
 
 function revisionHash () {
   return gulp.src([
-      `${workingDir}**/*.{css,css.map,js,js.map}`,
-      `${workingDir}**/*.{gif,ico,jpg,png,svg,webp}`,
-      `${workingDir}**/*.{woff,woff2}`,
+      `${workingDir}/**/*.{css,css.map,js,js.map}`,
+      `${workingDir}/**/*.{gif,ico,jpg,png,svg,webp}`,
+      `${workingDir}/**/*.{woff,woff2}`,
     ])
     .pipe(filter(file => !pattern.test(file.path)))       // prevent files from being revisioned twice
     .pipe(filter(file => !file.path.endsWith('/sw.js')))  // service workers must always be unrevisioned
@@ -32,10 +34,10 @@ function revisionHash () {
 }
 
 function revisionReplace () {
-  const manifestPath = `${workingDir}${manifestFilename}`
+  const manifestPath = path.join(workingDir, manifestFilename)
   return gulp.src([
-      `${workingDir}**/*.html`,
-      `${workingDir}sw.js`,
+      `${workingDir}/**/*.html`,
+      `${workingDir}/sw.js`,
     ])
     .pipe(filter(file => !pattern.test(file.path)))       // exclude revisioned files as their content must not change
     .pipe(revReplace({                                    // replace referenced to revisioned files
